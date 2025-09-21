5bc4a4fd0eabbb21ec7b47db051d5135fa454cc6
Ahtashamali1724 <hyahtasham@gmail.com>
2025-09-21 19:32:43 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Middleware server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
